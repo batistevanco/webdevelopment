26fd432af144ad9323ca3cf5ad494716362b3299
batistevanco <batiste.vancoillie@student.vives.be>
2025-02-21 09:04:20 +0100

    update script man van an Labo 4 finished

--- labo_4/opdrachtManVanAn/scripts/script.js
+++ labo_4/opdrachtManVanAn/scripts/script.js
@@ -1,24 +1,33 @@
-let tekst = "De man van An geeft geen hand aan ambetante verwanten";
-let zoektekst = "an";
-let count = 0;
-let positie = 0;
+const setup = () => {
+    zoekMetIndexOf();
+    zoekMetLastIndexOf();
+};
 
-while ((positie = tekst.indexOf(zoektekst, positie)) !== -1) {
-    count++;
-    positie += zoektekst.length; 
-}
+const zoekMetIndexOf = () => {
+    let tekst = "De man van An geeft geen hand aan ambetante verwanten";
+    let zoektekst = "an";
+    let count = 0;
+    let positie = 0;
 
-console.log("Aantal keer 'an' met indexOf:", count);
+    while ((positie = tekst.indexOf(zoektekst, positie)) !== -1) {
+        count++;
+        positie += zoektekst.length;
+    }
 
+    console.log("Aantal keer 'an' met indexOf:", count);
+};
 
-let tekst2 = "De man van An geeft geen hand aan ambetante verwanten";
-let zoektekst2 = "an";
-let count2 = 0;
-let positie2 = tekst2.length;
+const zoekMetLastIndexOf = () => {
+    let tekst2 = "De man van An geeft geen hand aan ambetante verwanten";
+    let zoektekst2 = "an";
+    let count2 = 0;
+    let positie2 = tekst2.length;
 
-while ((positie2 = tekst2.lastIndexOf(zoektekst2, positie2 - 1)) !== -1) {
-    count2++;
-}
+    while ((positie2 = tekst2.lastIndexOf(zoektekst2, positie2 - 1)) !== -1) {
+        count2++;
+    }
 
-console.log("Aantal keer 'an' met lastIndexOf:", count2);
+    console.log("Aantal keer 'an' met lastIndexOf:", count2);
+};
 
+window.addEventListener("load", setup);
